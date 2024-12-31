eb4cf9ad1b8b9a47975000f1eb0c7d98bf563bb0
ofins <jkyeewang@gmail.com>
2025-01-01 02:00:23 +0800

    fix: timer

--- my_module/close_all_positions.py
+++ my_module/close_all_positions.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from ib_insync import MarketOrder
 
 from my_module.logger import Logger
@@ -8,21 +10,17 @@ logger = Logger.get_logger(__name__)
 
 async def close_all_positions(ib):
     """
-    Closes all active positions.
+    Closes all active positions with MARKET order.
     """
     try:
         logger.info("Cancelling all outstanding orders...")
         ib.reqGlobalCancel()
-
         for pos in ib.positions():  # Exit all active trades
             contract = pos.contract
             contract.exchange = "SMART"
-            # order = MarketOrder(
-            #     "SELL" if pos.position > 0 else "BUY", abs(pos.position)
-            # )
-            # trade = ib.placeOrder(contract, order)
             action = "SELL" if pos.position > 0 else "BUY"
-            trade = place_order(ib, contract, action, "MARKET", abs(pos.position))
+            trade = place_order(ib, contract, action, abs(pos.position), "MARKET")
+
             while not trade.isDone():
                 await asyncio.sleep(1)
             logger.info(
